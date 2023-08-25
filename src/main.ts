8479dab087d14ef8375c2a39fab3bbbcc715277c
Xaibe <sa30967965@gmail.com>
2023-08-25 13:43:18 +0500

    functionality completed

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api', app, document);
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port 80");
+  });
 }
 bootstrap();
